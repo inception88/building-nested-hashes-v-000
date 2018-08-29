e0da9a5b2087f194806414dd0597cd95e56750f9
Benjamin Jones <bljones36@gmail.com>
2018-08-29 02:47:12 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -5,8 +5,11 @@ def second_challenge
       :patriarch => {},
       :matriarch => {},
       :hero => {},
-      :hero_friends => []
+      :hero_friends => []},
     :capulet => {
-      #your key/value pairs here!
-      }}
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
+   }}
 end
